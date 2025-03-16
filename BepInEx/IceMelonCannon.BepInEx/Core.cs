93631cab0c70635d4e55a840424be44e7217bbe4
Infinite-75 <czh767@163.com>
2025-03-16 23:31:51 +0800

    2.3.1-1.2

---
+++ BepInEx/IceMelonCannon.BepInEx/Core.cs
@@ -0,0 +1,89 @@
+﻿using CustomizeLib;
+using HarmonyLib;
+using Il2CppInterop.Runtime;
+using Il2CppInterop.Runtime.Injection;
+using BepInEx;
+using System.Reflection;
+using UnityEngine;
+using BepInEx.Unity.IL2CPP;
+
+namespace IceMelonCannon.BepInEx
+{
+    [HarmonyPatch(typeof(CannonBullet))]
+    public static class CannonBulletPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("HitLand")]
+        public static bool PostHitLand(CannonBullet __instance)
+        {
+            if (__instance.theBulletType == IceMelonCannon.BulletId)
+            {
+                CreateParticle.SetParticle(56, new(__instance.cannonPos.x, __instance.cannonPos.y), __instance.theBulletRow);
+                var pos = __instance.transform.position;
+                LayerMask layermask = __instance.zombieLayer.m_Mask;
+                var array = Physics2D.OverlapCircleAll(new(pos.x, pos.y), 1.5f);
+                foreach (var z in array)
+                {
+                    if (z is not null && z.gameObject.TryGetComponent<Zombie>(out var zombie) && !TypeMgr.IsAirZombie(zombie.theZombieType) && !zombie.isMindControlled)
+                    {
+                        zombie.TakeDamage(DmgType.IceAll, __instance.theBulletDamage);
+                        zombie.AddfreezeLevel(50);
+                        zombie.SetCold(15);
+                    }
+                }
+                GameAPP.PlaySound(UnityEngine.Random.RandomRangeInt(104, 106));
+                __instance.Die();
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [BepInPlugin("inf75.icemeloncannon", "IceMelonCannon", "1.0")]
+    public class Core : BasePlugin
+    {
+        public override void Load()
+        {
+            var ab = CustomCore.GetAssetBundle(Assembly.GetExecutingAssembly(), "icemeloncannon");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            ClassInjector.RegisterTypeInIl2Cpp<IceMelonCannon>();
+            CustomCore.RegisterCustomPlant<MelonCannon, IceMelonCannon>(1903, ab.GetAsset<GameObject>("IceMelonCannonPrefab"),
+                ab.GetAsset<GameObject>("IceMelonCannonPreview"), [(1132, 10)], 24, 30, 120, 1000, 30f, 900);
+            CustomCore.TypeMgrExtra.IsIcePlant.Add((PlantType)1903);
+            CustomCore.TypeMgrExtra.DoubleBoxPlants.Add((PlantType)1903);
+            CustomCore.RegisterCustomBullet<MelonBullet_cannon>(IceMelonCannon.BulletId, ab.GetAsset<GameObject>("ProjectileCannon_wintermelon"));
+            CustomCore.AddPlantAlmanacStrings(1903, "冰瓜加农炮", "手动发射大量冰瓜砸向僵尸\n<color=#3D1400>贴图作者：@屑红leong @仨硝基甲苯 @摆烂的克莱尔</color>\n<color=#3D1400>伤害：</color><color=red>120*40，3×3无衰减，受到伤害的僵尸增加50点冻结值，子弹落点为5×3随机</color>\n<color=#3D1400>装填时间：</color><color=red>24秒</color>\n<color=#3D1400>融合配方：</color><color=red>西瓜加农炮+寒冰菇</color>\n<color=#3D1400>“冰瓜炮曾经是我最好的战友，直到他讲出比寒冰毁灭菇还要冷的笑话，他离我而去，我则被他冰冻。”说完冰炮跑回战场，而冰瓜炮正在举办冰西瓜大胃王比赛。其他植物表示，为什么冰瓜炮的西瓜比其他的还要更冷，但实际上，冰瓜炮是不小心喝到液氮，才变成这副模样。</color>");
+        }
+    }
+
+    public class IceMelonCannon : MonoBehaviour
+    {
+        public IceMelonCannon() : base(ClassInjector.DerivedConstructorPointer<IceMelonCannon>()) => ClassInjector.DerivedConstructorBody(this);
+
+        public IceMelonCannon(IntPtr i) : base(i)
+        {
+        }
+
+        public void AnimShooting()
+        {
+            GameAPP.PlaySound(4, 1.0f);
+            var RowFromY = Mouse.Instance.GetRowFromY(plant.target.x, plant.target.y);
+            var bullet = plant.board.GetComponent<CreateBullet>().SetBullet(plant.shoot.transform.position.x, plant.shoot.transform.position.y, RowFromY, BulletId, 14);
+            var pos2 = bullet.cannonPos;
+            pos2.x = plant.target.x;
+            pos2.y = plant.target.y;
+            bullet.cannonPos = pos2;
+            bullet.rb.velocity = new(1.5f, 0);
+            bullet.specitalType = BulletStatus.Melon_cannon;
+            bullet.theBulletDamage = plant.attackDamage;
+        }
+
+        public void Awake()
+        {
+            plant.shoot = gameObject.transform.FindChild("Shoot");
+        }
+
+        public static int BulletId { get; set; } = 127;
+        public MelonCannon plant => gameObject.GetComponent<MelonCannon>();
+    }
+}
--- BepInEx/SuperGarlicFume.BepInEx/Core.cs
+++ BepInEx/SuperGarlicFume.BepInEx/Core.cs
@@ -22,7 +22,9 @@ namespace SuperGarlicFume.BepInEx
                 ab.GetAsset<GameObject>("SuperGarlicFumePreview"), [(904, 29)], 3, 0, 150, 300, 30, 700);
             CustomCore.AddFusion(904, 965, 8);
             CustomCore.TypeMgrExtra.IsIcePlant.Add((PlantType)965);
-            CustomCore.AddPlantAlmanacStrings(965, "究极蒜大喷菇", "究极蒜大喷菇的孢子能同时造成减速和中毒效果。\n<color=#3D1400>贴图作者：@林秋AutumnLin</color>\n<color=#3D1400>特点：</color><color=red>究极大喷菇亚种，使用大蒜、魅惑菇切换。持续攻击，每0.5s对本行所有僵尸造成150+蒜值*20伤害并减速，同时对每个受到攻击的僵尸附加10点冻结值和1点蒜值</color>\n<color=#3D1400>融合配方：</color><color=red>究极大喷菇+大蒜</color>\n<color=#3D1400>在经历一切的事情后，他决定退休，看一些老电影，欣赏向日葵歌剧，甚至是安排退休生活，不过僵尸不会给他机会。</color>");
+            SuperGarlicFume.Buff1 = CustomCore.RegisterCustomBuff("满屋蒜香：究极蒜大喷菇攻击时取消僵尸蒜值上限", BuffType.UltimateBuff, (PlantType)965, ab.GetAsset<Sprite>("SuperGarlicFume_0"));
+            SuperGarlicFume.Buff2 = CustomCore.RegisterCustomBuff("超剧毒：究极蒜大喷菇可以对僵尸额外造成总血量*蒜值%的伤害", BuffType.UltimateBuff, (PlantType)965, ab.GetAsset<Sprite>("SuperGarlicFume_0"));
+            CustomCore.AddPlantAlmanacStrings(965, "究极蒜大喷菇", "究极蒜大喷菇的孢子能同时造成减速和中毒效果。\n<color=#3D1400>贴图作者：@林秋AutumnLin</color>\n<color=#3D1400>特点：</color><color=red>究极大喷菇亚种，使用大蒜、魅惑菇切换。持续攻击，每0.5s对本行所有僵尸造成150+蒜值*20伤害并减速，同时对每个受到攻击的僵尸附加10点冻结值和1点蒜值</color>\n<color=#3D1400>融合配方：</color><color=red>究极大喷菇+大蒜</color>\n<color=#3D1400>\n<color=#3D1400>词条1：</color><color=red>凛风刺骨：攻击力×3</color>\n<color=#3D1400>词条2：</color><color=red>三尺之寒：冻结值积累速度×5</color>\n<color=#3D1400>词条3：</color><color=red>满屋蒜香：究极蒜大喷菇攻击时取消僵尸蒜值上限</color>\n<color=#3D1400>词条4：</color><color=red>超剧毒：究极蒜大喷菇可以对僵尸额外造成总血量*蒜值%的真实伤害</color>\n<color=#3D1400>在经历一切的事情后，他决定退休，看一些老电影，欣赏向日葵歌剧，甚至是安排退休生活，不过僵尸不会给他机会。</color>");
         }
     }
 
@@ -67,13 +69,27 @@ namespace SuperGarlicFume.BepInEx
             {
                 if (z is not null && !z.IsDestroyed() && !z.isMindControlled)
                 {
-                    z.TakeDamage(DmgType.Ice, (150 + z.poisonLevel * 20) * (Lawnf.TravelUltimate(4) ? 3 : 1));
-                    z.AddPoisonLevel();
+                    z.TakeDamage(DmgType.IceAll, (150 + z.poisonLevel * 20 * (Lawnf.TravelUltimate(4) ? 3 : 1)));
+                    if (Lawnf.TravelUltimate(Buff2))
+                    {
+                        z.TakeDamage(DmgType.IceAll, z.theMaxHealth * z.poisonLevel / 100, true);
+                    }
+                    if (Lawnf.TravelUltimate(Buff1))
+                    {
+                        z.SetPoison(10);
+                    }
+                    else
+                    {
+                        z.AddPoisonLevel();
+                    }
                     z.SetCold(10);
                     z.AddfreezeLevel(10 * (Lawnf.TravelUltimate(5) ? 5 : 1));
                 }
             }
         }
+
+        public static int Buff1 { get; set; } = -1;
+        public static int Buff2 { get; set; } = -1;
 
         public UltimateFume plant => gameObject.GetComponent<UltimateFume>();
     }

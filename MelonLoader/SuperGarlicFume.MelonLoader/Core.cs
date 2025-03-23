4da0f9767f3106d4c1dcac4b9a9ec986b8dfa808
Infinite-75 <czh767@163.com>
2025-03-23 14:18:33 +0800

    2.3.1-1.3

--- MelonLoader/SuperGarlicFume.MelonLoader/Core.cs
+++ MelonLoader/SuperGarlicFume.MelonLoader/Core.cs
@@ -1,13 +1,10 @@
 ﻿using CustomizeLib;
-using HarmonyLib;
-using Il2CppInterop.Runtime;
 using Il2CppInterop.Runtime.Injection;
 using MelonLoader;
 using SuperGarlicFume.MelonLoader;
 using System.Reflection;
 using Unity.VisualScripting;
 using UnityEngine;
-using static MelonLoader.MelonLogger;
 
 [assembly: MelonInfo(typeof(Core), "SuperGarlicFume", "1.0", "Infinite75", null)]
 [assembly: MelonGame("LanPiaoPiao", "PlantsVsZombiesRH")]
@@ -25,9 +22,10 @@ namespace SuperGarlicFume.MelonLoader
                 ab.GetAsset<GameObject>("SuperGarlicFumePreview"), [(904, 29)], 3, 0, 150, 300, 30, 700);
             CustomCore.AddFusion(904, 965, 8);
             CustomCore.TypeMgrExtra.IsIcePlant.Add((PlantType)965);
-            SuperGarlicFume.Buff1 = CustomCore.RegisterCustomBuff("满屋蒜香：究极蒜大喷菇攻击时取消僵尸蒜值上限", BuffType.UltimateBuff, (PlantType)965, ab.GetAsset<Sprite>("SuperGarlicFume_0"));
-            SuperGarlicFume.Buff2 = CustomCore.RegisterCustomBuff("超剧毒：究极蒜大喷菇可以对僵尸额外造成总血量*蒜值%的伤害", BuffType.UltimateBuff, (PlantType)965, ab.GetAsset<Sprite>("SuperGarlicFume_0"));
-            CustomCore.AddPlantAlmanacStrings(965, "究极蒜大喷菇", "究极蒜大喷菇的孢子能同时造成减速和中毒效果。\n<color=#3D1400>贴图作者：@林秋AutumnLin</color>\n<color=#3D1400>特点：</color><color=red>究极大喷菇亚种，使用大蒜、魅惑菇切换。持续攻击，每0.5s对本行所有僵尸造成150+蒜值*20伤害并减速，同时对每个受到攻击的僵尸附加10点冻结值和1点蒜值</color>\n<color=#3D1400>融合配方：</color><color=red>究极大喷菇+大蒜</color>\n<color=#3D1400>\n<color=#3D1400>词条1：</color><color=red>凛风刺骨：攻击力×3</color>\n<color=#3D1400>词条2：</color><color=red>三尺之寒：冻结值积累速度×5</color>\n<color=#3D1400>词条3：</color><color=red>满屋蒜香：究极蒜大喷菇攻击时取消僵尸蒜值上限</color>\n<color=#3D1400>词条4：</color><color=red>超剧毒：究极蒜大喷菇可以对僵尸额外造成总血量*蒜值%的真实伤害</color>\n<color=#3D1400>在经历一切的事情后，他决定退休，看一些老电影，欣赏向日葵歌剧，甚至是安排退休生活，不过僵尸不会给他机会。</color>");
+            SuperGarlicFume.Buff1 = CustomCore.RegisterCustomBuff("满屋蒜香：究极蒜大喷菇攻击时取消僵尸蒜值上限", BuffType.AdvancedBuff, () => Lawnf.TravelUltimate(4) && Lawnf.TravelUltimate(5) && Board.Instance.ObjectExist<SuperGarlicFume>(), 5400, "#DA64FF", (PlantType)965);
+            SuperGarlicFume.Buff2 = CustomCore.RegisterCustomBuff("超剧毒：究极蒜大喷菇可以对僵尸额外造成总血量×蒜值%的伤害", BuffType.AdvancedBuff, () => Lawnf.TravelUltimate(4) && Lawnf.TravelUltimate(5) && Lawnf.TravelAdvanced(SuperGarlicFume.Buff1) && Board.Instance.ObjectExist<SuperGarlicFume>(), 17900, "red", (PlantType)965);
+            SuperGarlicFume.Buff3 = CustomCore.RegisterCustomBuff("冰蒜天：究极蒜大喷菇可以在僵尸被冻结时使僵尸蒜值增加e的(本行究极投手总数+1)次方，对僵尸本体额外造成(蒜值×(本行究极投手总数+1)的三次方)伤害", BuffType.AdvancedBuff, () => Lawnf.TravelUltimate(4) && Lawnf.TravelUltimate(5) && Lawnf.TravelAdvanced(SuperGarlicFume.Buff2) && Board.Instance.ObjectExist<SuperGarlicFume>(), 17900, "red", (PlantType)965);
+            CustomCore.AddPlantAlmanacStrings(965, "究极蒜大喷菇", "究极蒜大喷菇的孢子能同时造成减速和中毒效果。\n<color=#3D1400>贴图作者：@林秋AutumnLin</color>\n<color=#3D1400>特点：</color><color=red>究极大喷菇亚种，使用大蒜、魅惑菇切换。持续攻击，每0.5s对本行所有僵尸造成150+蒜值*20伤害并减速，同时对每个受到攻击的僵尸附加10点冻结值和1点蒜值</color>\n<color=#3D1400>融合配方：</color><color=red>究极大喷菇+大蒜</color>\n<color=#3D1400>\n<color=#3D1400>词条1：</color><color=red>凛风刺骨：攻击力×3</color>\n<color=#3D1400>词条2：</color><color=red>三尺之寒：冻结值积累速度×5</color>\n<color=#3D1400>词条3：</color><color=red>满屋蒜香：究极蒜大喷菇攻击时取消僵尸蒜值上限(解锁条件：解锁了词条1、2且场上存在究极蒜大喷菇)</color>\n<color=#3D1400>词条4：</color><color=red>超剧毒：究极蒜大喷菇可以对僵尸额外造成总血量*蒜值%的真实伤害(解锁条件：词条3的所有条件+解锁词条3)</color>\n<color=#3D1400>词条5：</color><color=red>冰蒜天：究极蒜大喷菇可以在僵尸被冻结时使僵尸蒜值增加e的(本行究极投手总数+1)次方，对僵尸本体额外造成(蒜值×(本行究极投手总数+1)的三次方)伤害(解锁条件：词条4的所有条件+解锁词条4)</color>\n<color=#3D1400>在经历一切的事情后，他决定退休，看一些老电影，欣赏向日葵歌剧，甚至是安排退休生活，不过僵尸不会给他机会。</color>");
         }
     }
 
@@ -73,12 +71,13 @@ namespace SuperGarlicFume.MelonLoader
             {
                 if (z is not null && !z.IsDestroyed() && !z.isMindControlled)
                 {
-                    z.TakeDamage(DmgType.IceAll, (150 + z.poisonLevel * 20 + z.theMaxHealth * (Lawnf.TravelUltimate(Buff2) ? z.poisonLevel / 100 : 0)) * (Lawnf.TravelUltimate(4) ? 3 : 1));
-                    if (Lawnf.TravelUltimate(Buff2))
+                    z.TakeDamage(DmgType.IceAll, (150 + z.poisonLevel * 20 + z.theMaxHealth * (Lawnf.TravelAdvanced(Buff2) ? z.poisonLevel / 100 : 0)) * (Lawnf.TravelUltimate(4) ? 3 : 1));
+                    GameAPP.PlaySound(UnityEngine.Random.RandomRangeInt(0, 3));
+                    if (Lawnf.TravelAdvanced(Buff2))
                     {
                         z.TakeDamage(DmgType.IceAll, z.theMaxHealth * z.poisonLevel / 100, true);
                     }
-                    if (Lawnf.TravelUltimate(Buff1))
+                    if (Lawnf.TravelAdvanced(Buff1))
                     {
                         z.SetPoison(10);
                     }
@@ -88,13 +87,30 @@ namespace SuperGarlicFume.MelonLoader
                     }
                     z.SetCold(10);
                     z.AddfreezeLevel(10 * (Lawnf.TravelUltimate(5) ? 5 : 1));
+
+                    if (z.poisonLevel <= 1000000 && Lawnf.TravelAdvanced(Buff3) && z.theFreezeCountDown > 3)
+                    {
+                        double multiplier = 1;
+                        foreach (var p in Board.Instance.plantArray)
+                        {
+                            if (p is not null && p.thePlantRow == plant.thePlantRow && p.TryCast<UltimateMelon>() is not null)
+                            {
+                                multiplier++;
+                            }
+                        }
+                        double newPoison = z.poisonLevel + Math.Pow(Math.E, multiplier);
+                        if (newPoison > int.MaxValue / 2) newPoison = int.MaxValue / 2;
+                        z.theFreezeCountDown = 3;
+                        z.poisonLevel = (int)newPoison;
+                        z.BodyTakeDamage((int)(multiplier * multiplier * multiplier * z.poisonLevel));
+                    }
                 }
             }
         }
 
         public static int Buff1 { get; set; } = -1;
         public static int Buff2 { get; set; } = -1;
-
+        public static int Buff3 { get; set; } = -1;
         public UltimateFume plant => gameObject.GetComponent<UltimateFume>();
     }
 }
